0bbd992e1eb686408bc790af1624c33fa280fc2f
Babylon <leonardo.m.garduno@gmail.com>
2021-05-29 18:45:57 -0500

    flask import

--- app.py
+++ app.py
@@ -1,20 +1,51 @@
 
+
+#################################################
+# Import dependencies
+#################################################   
+
+import numpy as np
+import pandas as pd
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from sqlalchemy import inspect
 
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-#engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
+# Save reference to the tables
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def home():
+    return (
+        f"Welcome to the SQLAlchemy Weather - Surfs Up!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation <br/>"
+        f"/api/v1.0/stations <br/>"
+        f"/api/v1.0/tobs <br/>"
+        f"/api/v1.0/start/<start> <br/>"
+        f"/api/v1.0/start-end/<start>/<end> <br/>"
+    )
